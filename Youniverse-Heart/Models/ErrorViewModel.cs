6ba6cb0cc8a5c4b37513428fb2c71f96f599e5b5
John Naughton <Jnaughton29@gmail.com>
2023-11-29 17:24:37 +0000

    initial commit

---
+++ Youniverse-Heart/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Youniverse_Heart.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
